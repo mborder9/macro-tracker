9cce09d54337ec20f3c40e47a3a67c46887b31e4
mborder <mborder@mail.uoguelph.ca>
2017-09-20 21:04:10 -0400

    Adding new food data form

--- client/templates/food/food_list.js
+++ client/templates/food/food_list.js
@@ -1,5 +1,18 @@
 Template.foodList.helpers({
 	foods: function() {
 		return Foods.find();
+	},
+	addNewEntry: function(){
+		return Session.get('addNewEntry');
 	}
+});
+
+Template.foodList.events({
+	'click #add-btn' (event) {
+		Session.set('addNewEntry', true);
+	}
+});
+
+Template.weeklyTrack.onCreated(function() {
+	Session.set('addNewEntry', false);
 });
