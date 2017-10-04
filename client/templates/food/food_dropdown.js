e5b7600a31f73d250c7e51ce9f3ebef41b29115b
mborder <mborder@mail.uoguelph.ca>
2017-10-03 22:46:54 -0400

    Load existing saved food log from database.

--- client/templates/food/food_dropdown.js
+++ client/templates/food/food_dropdown.js
@@ -1,5 +1,9 @@
 Template.foodDropdown.helpers({
 	foods: function() {
 		return Foods.find();
+	},
+	getCurrentValue: function(row, type) {
+		console.log(Session.get(type+"_"+row));
+		return Session.get(type+"_"+row);
 	}
 });
--- client/templates/food/weekly_tracker.js
+++ client/templates/food/weekly_tracker.js
@@ -35,6 +35,87 @@ function deleteRow(meal, row, numRows){
     
 }
 
+function getData(meal, data){
+    for(var i = 0; i<data.length; i++){
+    	console.log("Setting "+meal+"_"+i+" equal to: "+data[i].food);
+		Session.set(meal+'_'+i, data[i].food);
+		console.log("Setting "+meal+"_"+i+"_quantity equal to: "+data[i].quantity);
+		Session.set(meal+'_'+i+'_quantity', data[i].quantity);
+		setNutrition(meal,i,data[i].food);
+	}
+    
+	
+}
+
+function eraseData(meal){
+	var rows = Session.get(meal+"_NumberRows");
+	for (var i = 0; i < rows; i ++){
+		Session.set(meal+'_'+i, '');
+		Session.set(meal+'_'+i+'_protein', 0);
+		Session.set(meal+'_'+i+'_carbs', 0);
+		Session.set(meal+'_'+i+'_fats', 0);
+		Session.set(meal+'_'+i+'_calories', 0);
+	}
+	Session.set(meal+"_NumberRows",0);
+}
+function loadData(){
+	var data = userHistory.findOne({user: Meteor.user()._id, date: Session.get('currentDate')});
+	console.log(data);
+	if(data){
+		var breakfast = data.breakfast;
+		if(breakfast){
+			Session.set('breakfast_NumberRows', breakfast.length);
+			getData("breakfast", breakfast);
+			updateSum("breakfast");
+		}else {
+			eraseData("breakfast");
+			updateSum("breakfast");
+		}
+
+		var snacks = data.snacks;
+		if(snacks){
+			Session.set('snacks_NumberRows', snacks.length);
+			getData("snacks", snacks);
+			updateSum("snacks");
+		}else {
+			eraseData("snacks");
+			updateSum("snacks");
+		}
+
+		var lunch = data.lunch;
+		if(lunch){
+			Session.set('lunch_NumberRows', lunch.length);
+			getData("lunch", lunch);
+			updateSum("lunch");
+		}else {
+			eraseData("lunch");
+			updateSum("lunch");
+		}
+
+		var dinner = data.dinner;
+		if(dinner){
+			Session.set('dinner_NumberRows', dinner.length);
+			getData("dinner", dinner);
+			updateSum("dinner");
+		}else {
+			eraseData("dinner");
+			updateSum("dinner");
+		}
+	}else {
+			console.log("ERASE DATA");
+			eraseData("breakfast");
+			eraseData("snacks");
+			eraseData("lunch");
+			eraseData("dinner");
+
+			updateSum("breakfast");
+			updateSum("snacks");
+			updateSum("lunch");
+			updateSum("dinner");
+	}
+	
+}
+
 function saveData(){
 	var data = userHistory.findOne({user: Meteor.user()._id, date: Session.get('currentDate')});
 	userHistory.remove({_id: data._id});
@@ -101,6 +182,93 @@ function getCurrentDate(){
 	return today;
 }
 
+function incrementDate(){
+	var currentDate = Session.get('currentDate');
+	var nextDate = new Date(currentDate);
+	nextDate.setDate(nextDate.getDate() + 1);
+	console.log("DAte: "+nextDate);
+	var dd = nextDate.getDate();
+	var mm = nextDate.getMonth()+1; //January is 0!
+
+	var yyyy = nextDate.getFullYear();
+	if(dd<10){
+		dd='0'+dd;
+	} 
+	if(mm<10){
+			mm='0'+mm;
+	} 
+	var nextDate = mm+'/'+dd+'/'+yyyy;
+	Session.set('currentDate', nextDate);
+}
+
+function decrementDate(){
+	var currentDate = Session.get('currentDate');
+	var nextDate = new Date(currentDate);
+	nextDate.setDate(nextDate.getDate() - 1);
+	console.log("DAte: "+nextDate);
+	var dd = nextDate.getDate();
+	var mm = nextDate.getMonth()+1; //January is 0!
+
+	var yyyy = nextDate.getFullYear();
+	if(dd<10){
+		dd='0'+dd;
+	} 
+	if(mm<10){
+			mm='0'+mm;
+	} 
+	var nextDate = mm+'/'+dd+'/'+yyyy;
+	Session.set('currentDate', nextDate);
+}
+
+function updateSum(meal){
+	console.log("Update sum");
+	var nutrients = ["protein", "carbs", "fats", "calories"];
+	nutrients.forEach(function(nutrient) {
+        console.log(nutrient);
+		var rows = Session.get(meal+'_NumberRows');
+		console.log(rows);
+		var sum = 0;
+		var nut = 0;
+		var quantity = 0;
+		for(var i=0; i<rows; i++){
+			console.log('row' + i + ':' + nutrient + ' - ' + Session.get(meal+'_'+i+'_'+nutrient))
+			nut = parseFloat(Session.get(meal+'_'+i+'_'+nutrient));
+			quantity = parseFloat(Session.get(meal+'_'+i+'_quantity'));
+			if(isNaN(quantity)){
+				quantity = 0;
+			}
+			if(typeof Session.get(meal+'_'+i+'_'+nutrient) !== 'undefined'){
+				sum = sum + (quantity*nut);
+			}
+			
+		}
+		if(isNaN(sum)){
+			Session.set(meal+"_"+nutrient+'_sum', 0);
+		} else {
+			Session.set(meal+"_"+nutrient+'_sum',sum.toFixed(2));
+		}   
+    });	
+    updateTotalSum();
+}
+
+function updateTotalSum(){
+	var nutrients = ["protein", "carbs", "fats", "calories"];
+	nutrients.forEach(function(nutrient) {
+
+		var BreakfastSum = parseFloat(Session.get("breakfast_"+nutrient+"_sum"));
+		console.log("Breakfast sum = "+BreakfastSum);
+		var SnacksSum = parseFloat(Session.get("snacks_"+nutrient+"_sum"));
+		console.log("Snack sum = "+SnacksSum);
+		var LunchSum = parseFloat(Session.get("lunch_"+nutrient+"_sum"));
+		var DinnerSum = parseFloat(Session.get("dinner_"+nutrient+"_sum"));
+
+		var total = BreakfastSum + SnacksSum + LunchSum + DinnerSum;
+
+		Session.set("total_"+nutrient, total);
+		console.log("Total for "+nutrient+" = "+total);
+	});
+}
+
 Template.foodDropdown.events({
 	'change .dropdown_breakfast' (event) {
 		console.log("Changing dropdown_breakfast");
@@ -109,6 +277,7 @@ Template.foodDropdown.events({
 		var meal = "breakfast";
 		var value = event.target.value;
 		setNutrition(meal,row,value);
+	
 	},
 	'change .dropdown_snacks' (event) {
 		var id = event.currentTarget.id;
@@ -116,6 +285,7 @@ Template.foodDropdown.events({
 		var meal = "snacks";
 		var value = event.target.value;
 		setNutrition(meal,row,value);
+		
 	},
 	'change .dropdown_lunch' (event) {
 		var id = event.currentTarget.id;
@@ -123,6 +293,7 @@ Template.foodDropdown.events({
 		var meal = "lunch";
 		var value = event.target.value;
 		setNutrition(meal,row,value);
+		
 	},
 	'change .dropdown_dinner' (event) {
 		var id = event.currentTarget.id;
@@ -130,6 +301,7 @@ Template.foodDropdown.events({
 		var meal = "dinner";
 		var value = event.target.value;
 		setNutrition(meal,row,value);
+	
 	}
 });
 
@@ -172,25 +344,40 @@ Template.weeklyTrack.events({
 		var type = id.split(/_/)[2];
 		console.log("Setting quantity of "+type+" row"+num+" to"+event.currentTarget.value);
     	Session.set(type+'_'+num+'_quantity', event.currentTarget.value);
+    	updateSum(type);
   	},
   	'click #breakfast-delete' (event){
   		var rows = Session.get('breakfast_NumberRows');
 		deleteRow("breakfast",this, rows);
+		updateSum("breakfast");
   	},
   	'click #snacks-delete' (event){
   		var rows = Session.get('snacks_NumberRows');
 		deleteRow("snacks",this, rows);
+		updateSum("snacks");
   	},
   	'click #lunch-delete' (event){
   		var rows = Session.get('lunch_NumberRows');
 		deleteRow("lunch",this, rows);
+		updateSum("lunch");
   	},
   	'click #dinner-delete' (event){
   		var rows = Session.get('dinner_NumberRows');
 		deleteRow("dinner",this, rows);
+		updateSum("dinner");
   	},
   	'click #save-btn' (event){
   		saveData();
+  	},
+  	'click #right-btn': function(event, t){
+  		t.reload.set(true);
+  		incrementDate();
+  		loadData();
+  	},
+  	'click #left-btn': function(event, t){
+  		t.reload.set(true);
+  		decrementDate();
+  		loadData();
   	}
 });
 
@@ -235,46 +422,18 @@ Template.weeklyTrack.helpers({
 		return numArr;
 	},
 	getSum: function(nutrient, type){
-		console.log(nutrient);
-		var rows = Session.get(type+'_NumberRows');
-		console.log(rows);
-		var sum = 0;
-		var nut = 0;
-		var quantity = 0;
-		for(var i=0; i<rows; i++){
-			console.log('row' + i + ':' + nutrient + ' - ' + Session.get(type+'_'+i+'_'+nutrient))
-			nut = parseFloat(Session.get(type+'_'+i+'_'+nutrient));
-			quantity = parseFloat(Session.get(type+'_'+i+'_quantity'));
-			if(isNaN(quantity)){
-				quantity = 0;
-			}
-			if(typeof Session.get(type+'_'+i+'_'+nutrient) !== 'undefined'){
-				sum = sum + (quantity*nut);
-			}
-			
-		}
-		if(isNaN(sum)){
-			Session.set(type+nutrient+'_sum', 0);
-			return 0;
-		} else {
-			Session.set(type+"_"+nutrient+'_sum',sum.toFixed(2));
-			return sum.toFixed(2);
-		}
+		return Session.get(type+"_"+nutrient+'_sum');
 		
 	},
 	getTotalSum: function(nutrient){
-		var BreakfastSum = parseFloat(Session.get("breakfast_"+nutrient+"_sum"));
-		var SnacksSum = parseFloat(Session.get("snacks_"+nutrient+"_sum"));
-		var LunchSum = parseFloat(Session.get("lunch_"+nutrient+"_sum"));
-		var DinnerSum = parseFloat(Session.get("dinner_"+nutrient+"_sum"));
-
-		var total = BreakfastSum + SnacksSum + LunchSum + DinnerSum;
-
-		Session.set("total_"+nutrient, total);
-		return total.toFixed(2);
+		return Session.get("total_"+nutrient);
 	},
 	getCurrentDate: function(){
 		return Session.get("currentDate");
+	},
+	getCurrentQuantity: function(row, meal){
+		console.log(Session.get(meal+"_"+row+"_quantity"));
+		return Session.get(meal+"_"+row+"_quantity");
 	}
 });
 
@@ -309,8 +468,16 @@ Template.weeklyTrack.onCreated(function() {
 	Session.set('dinner_0_quantity',0);
 
 	Session.set('currentDate', getCurrentDate());
+	updateSum("breakfast");
+	updateSum("snacks");
+	updateSum("lunch");
+	updateSum("dinner");
     console.log('Template.body.onCreated');  
 });
+
+Template.weeklyTrack.created = function() {
+  this.reload = new ReactiveVar();
+};
 
 Template.weeklyTrack.destroyed = function(){
   Session.set('CurrentSelection', null);
